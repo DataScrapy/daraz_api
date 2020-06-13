9d6873b2421b46a8f31197d3594c36d925d14cdb
chintanvi-dev <chintanshahvi.dev@gmail.com>
2020-06-13 18:11:20 +0530

    tests

--- page_content_api.py
+++ page_content_api.py
@@ -131,7 +131,7 @@ def scrap(main_url):
 
         root_dir = os.path.dirname(os.path.abspath(__file__))
 
-        driver = webdriver.Chrome(options=options, executable_path=root_dir + '/chromedriver')
+        driver = webdriver.Chrome(options=options)
 
         driver.maximize_window()
 
